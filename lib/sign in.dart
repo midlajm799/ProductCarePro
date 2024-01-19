ed78acd2c9d3a2fdaee4ed1921bfb6e89c77fe81
midlajm799 <midlajm799@gmail.com>
2024-01-19 15:31:36 +0530

    first commit02

--- lib/sign in.dart
+++ lib/sign in.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SignIn extends StatelessWidget {
   SignIn({super.key});
-  TextEditingController emailcontroller=TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,57 +25,73 @@ class SignIn extends StatelessWidget {
                 width: 250,
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 60,right: 60),
-                child: TextFormField(controller:emailcontroller ,
-                  decoration: InputDecoration(filled: true,
-                    fillColor: Colors.grey[300],
-                    labelText: 'Email',labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0), // Add curved edges
-                      borderSide: BorderSide.none, // Remove the border
+                padding: const EdgeInsets.only(left: 60, right: 60),
+                child: SizedBox(
+                  height: 50,
+                  width: 340,
+                  child: TextFormField(
+                    controller: emailcontroller,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      labelText: 'Email',
+                      labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius:
+                            BorderRadius.circular(10.0), // Add curved edges
+                        borderSide: BorderSide.none, // Remove the border
+                      ),
                     ),
                   ),
                 ),
-
-
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 60,right: 60,top: 15),
-                child: TextFormField(
-                  decoration: InputDecoration(filled: true,
-                    fillColor: Colors.grey[300],
-                    labelText: 'password',labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0), // Add curved edges
-                      borderSide: BorderSide.none, // Remove the border
+                padding: const EdgeInsets.only(left: 60, right: 60, top: 15),
+                child: SizedBox(
+                  height: 50,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      labelText: 'password',
+                      labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius:
+                            BorderRadius.circular(10.0), // Add curved edges
+                        borderSide: BorderSide.none, // Remove the border
+                      ),
                     ),
                   ),
                 ),
-
-
-
               ),
-              ElevatedButton(
-                onPressed: () {
-
-                  print(emailcontroller.text);
-                  // Navigator.push(context, MaterialPageRoute(builder: (context)=>SignIn()));
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.orange, // Set the button color to orange
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                    BorderRadius.circular(30.0), // Add rounded corners
+              Padding(
+                padding: const EdgeInsets.only(top:15),
+                child: Text('not have an account?sign up'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print(emailcontroller.text);
+                    // Navigator.push(context, MaterialPageRoute(builder: (context)=>SignIn()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange, // Set the button color to orange
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(30.0), // Add rounded corners
+                    ),
                   ),
-                ),
-                child: Text("Sign In!",style:TextStyle(fontWeight: FontWeight.w700),
+                  child: Text(
+                    "Sign In!",
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
                 ),
               ),
             ],
           ),
         ),
       ),
-
     );
   }
 }
