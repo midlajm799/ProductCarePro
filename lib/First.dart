e72233f454c4e9fa289172b249fc52b00182c7dd
midlajm799 <midlajm799@gmail.com>
2024-01-19 12:06:59 +0530

    first commit4

---
+++ lib/First.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(bottom: 60),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 200),
+            Image.asset(
+              'assets/logo1.jpg',
+              height: 250,
+              width: 250,
+            ),
+            Stack(
+              children: [
+                Text(
+                  'PRODUCT',
+                  style: TextStyle(
+                      color: Color(0x33383C72),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 22),
+                  child: Text(
+                    'PRODUCT',
+                    style: TextStyle(
+                        color: Color(0x33383C72),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: Text(
+                    'PRODUCT',
+                    style: TextStyle(
+                        color: Color(0xFF383C72),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Text(
+                'CARE PRO.',
+                style: TextStyle(
+                    color: Color(0xFF383C72),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top:150),
+              child: Text(
+                """Stay Covered,Stay Connected
+                     With Us.""",
+                style: TextStyle(
+                    color: Color(0xFF1E1E1E),fontSize: 15
+                ),
+              ),
+            ),
+
+            Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                // Add your button press logic here
+                print('Button Pressed');
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange, // Set the button color to orange
+                shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(30.0), // Add rounded corners
+                ),
+              ),
+              child: Text("Let's Go!",style:TextStyle(fontWeight: FontWeight.w700),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
