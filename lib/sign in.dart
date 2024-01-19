1effada1630fa7fd6f64da6bfae51232b62ba229
midlajm799 <midlajm799@gmail.com>
2024-01-19 15:18:23 +0530

    first commit01

--- lib/sign in.dart
+++ lib/sign in.dart
@@ -1,17 +1,80 @@
 import 'package:flutter/material.dart';
 
 class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+  SignIn({super.key});
+  TextEditingController emailcontroller=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: Container(
+        margin: EdgeInsets.only(bottom: 60),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 200),
+              Image.asset(
+                'assets/logo2.jpg',
+                height: 250,
+                width: 250,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 60,right: 60),
+                child: TextFormField(controller:emailcontroller ,
+                  decoration: InputDecoration(filled: true,
+                    fillColor: Colors.grey[300],
+                    labelText: 'Email',labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0), // Add curved edges
+                      borderSide: BorderSide.none, // Remove the border
+                    ),
+                  ),
+                ),
 
 
-      )
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 60,right: 60,top: 15),
+                child: TextFormField(
+                  decoration: InputDecoration(filled: true,
+                    fillColor: Colors.grey[300],
+                    labelText: 'password',labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0), // Add curved edges
+                      borderSide: BorderSide.none, // Remove the border
+                    ),
+                  ),
+                ),
 
+
+
+              ),
+              ElevatedButton(
+                onPressed: () {
+
+                  print(emailcontroller.text);
+                  // Navigator.push(context, MaterialPageRoute(builder: (context)=>SignIn()));
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange, // Set the button color to orange
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                    BorderRadius.circular(30.0), // Add rounded corners
+                  ),
+                ),
+                child: Text("Sign In!",style:TextStyle(fontWeight: FontWeight.w700),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
 
     );
   }
