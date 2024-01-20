86ba355a964dff852264cacfa28da2613c6b4197
midlajm799 <midlajm799@gmail.com>
2024-01-20 06:54:51 +0530

    first commit04

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,44 +1,80 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 350, // Set the width of the container
-          height: 120, // Set the height of the container
-          decoration:BoxDecoration(color: Colors.red.shade900, // Set the color of the container
-            borderRadius: BorderRadius.circular(15.0), // Curve the corners
-          ),
-            alignment: Alignment.topLeft, // Align the text to the top-left
-            child: Padding(
-              padding: const EdgeInsets.all(10.0), // Add some padding
-              child: Text(
-                'Shop',
-                style: TextStyle(
-                  color: Colors.white, // Set the text color
-                  fontSize: 18, // Set the font size
-                  fontWeight: FontWeight.bold, // Set the font weight
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 350,
+              height: 120,
+              decoration: BoxDecoration(
+                color: Colors.red.shade900,
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Shop',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            )
+            ),
+            SizedBox(height: 20), // Add some space between the containers
+            Container(
+              width: 350,
+              height: 120,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Rgister Warranty',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20), // Add some space between the containers
+            Container(
+              width: 350,
+              height: 120,
+              decoration: BoxDecoration(
+                color: Colors.blue.shade900,
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Rgister Complaint',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-
       ),
-
-
-
-
-
-
-
-
-
-
-
     );
   }
 }
