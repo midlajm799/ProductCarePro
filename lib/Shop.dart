e2697cca06564116faff656b1bca878feb060631
midlajm799 <midlajm799@gmail.com>
2024-01-20 07:16:01 +0530

    first commit06

---
+++ lib/Shop.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sample/sign%20in.dart';
+
+class Shop extends StatelessWidget {
+  const Shop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+
+
+
+
+
+
+
+      
+    );
+  }
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sample/Shop.dart';
+import 'package:sample/sign%20in.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,64 +12,82 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 350,
-              height: 120,
-              decoration: BoxDecoration(
-                color: Colors.red.shade900,
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  'Shop',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            InkWell(
+              onTap: () { 
+                // Add your functionality for the "Shop" button
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>Shop()));
+              },
+              child: Container(
+                width: 350,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.red.shade900,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Shop',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
             ),
             SizedBox(height: 20), // Add some space between the containers
-            Container(
-              width: 350,
-              height: 120,
-              decoration: BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  'Register Warranty',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            InkWell(
+              onTap: () {
+                // Add your functionality for the "Register Warranty" button
+                print('Register Warranty button pressed');
+              },
+              child: Container(
+                width: 350,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Register Warranty',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
             ),
             SizedBox(height: 20), // Add some space between the containers
-            Container(
-              width: 350,
-              height: 120,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 4, 46, 108),
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  'Register Complaint',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            InkWell(
+              onTap: () {
+                // Add your functionality for the "Register Complaint" button
+                print('Register Complaint button pressed');
+              },
+              child: Container(
+                width: 350,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 4, 46, 108),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Register Complaint',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
