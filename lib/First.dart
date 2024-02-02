b411338dfcd5b84a30d8fe70fe6a14cee121b243
midlajm799 <midlajm799@gmail.com>
2024-02-02 10:03:35 +0530

    2

--- lib/First.dart
+++ lib/First.dart
@@ -1,154 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:sample/otp.dart';
 import 'package:sample/sign%20in.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class FirstPage1 extends StatelessWidget {
+  const FirstPage1({super.key});
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-=======
-    final Height=MediaQuery.of(context).size.height;
-    final Width=MediaQuery.of(context).size.width;
->>>>>>> origin/main
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(10),
-        height: height,
-        width: width,
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-<<<<<<< HEAD
-            Image.asset(
-              "assets/logo1.jpg",
-              height: height * .35,
-              width: width * .7,
-=======
-            SizedBox(height: Height*0.1),
-            Image.asset(
-              'assets/logo1.jpg',
-              height: Height*0.5,
-              width: Width*0.65,
->>>>>>> origin/main
-            ),
-            Stack(
+        body: Container(
+            margin: EdgeInsets.all(10),
+            height: height,
+            width: width,
+            color: Colors.white,
+            child: Column(mainAxisAlignment: MainAxisAlignment.end,
               children: [
+                Image.asset("assets/logo1.jpg",height: height*.35,width: width*.7,),
+                Stack(
+                  children: [
+                    Text(
+                      'PRODUCT',
+                      style: TextStyle(
+                          color: const Color(0x33383C72),
+                          fontWeight: FontWeight.bold,
+                          fontSize: height*.0399),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 22),
+                      child: Text(
+                        'PRODUCT',
+                        style: TextStyle(
+                            color: const Color(0x33383C72),
+                            fontWeight: FontWeight.bold,
+                            fontSize: height*.0399),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12),
+                      child: Text(
+                        'PRODUCT',
+                        style: TextStyle(
+                            color: const Color(0xFF383C72),
+                            fontWeight: FontWeight.bold,
+                            fontSize: height*.0399),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 58.0),
+                      child: Text(
+                        'CARE PRO.',
+                        style: TextStyle(
+                            color: const Color(0xFF383C72),
+                            fontWeight: FontWeight.bold,
+                            fontSize: height*.037),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: height*.15,),
                 Text(
-                  'PRODUCT',
-                  style: TextStyle(
-                      color: const Color(0x33383C72),
-                      fontWeight: FontWeight.bold,
-<<<<<<< HEAD
-                      fontSize: height * .0399),
-=======
-                      fontSize: Height*0.038),
->>>>>>> origin/main
+                  """Stay Covered,Stay Connected
+                   With Us.""",
+                  style: TextStyle(color: const Color(0xFF1E1E1E), fontSize: height*.016,fontWeight: FontWeight.w500),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 22),
-                  child: Text(
-                    'PRODUCT',
-                    style: TextStyle(
-                        color: const Color(0x33383C72),
-                        fontWeight: FontWeight.bold,
-<<<<<<< HEAD
-                        fontSize: height * .0399),
-=======
-                        fontSize:Height*0.038),
->>>>>>> origin/main
+                SizedBox(height: height*.008,),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => SignIn()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange, // Set the button color to orange
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                      BorderRadius.circular(height*.30), // Add rounded corners
+                    ),
+                  ),
+                  child: const Text(
+                    "Let's Go!",
+                    style: TextStyle(fontWeight: FontWeight.w700),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 12),
-                  child: Text(
-                    'PRODUCT',
-                    style: TextStyle(
-                        color: const Color(0xFF383C72),
-                        fontWeight: FontWeight.bold,
-<<<<<<< HEAD
-                        fontSize: height * .0399),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 58.0),
-                  child: Text(
-                    'CARE PRO.',
-                    style: TextStyle(
-                        color: const Color(0xFF383C72),
-                        fontWeight: FontWeight.bold,
-                        fontSize: height * .037),
-=======
-                        fontSize: Height*0.038),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom:  5,top: 50),
-                  child:
-                  Text(
-                    'CARE PRO.',
-                    style: TextStyle(
-                        color: Color(0xFF383C72),
-                        fontWeight: FontWeight.bold,
-                        fontSize: Height*0.036),
->>>>>>> origin/main
-                  ),
-                ),
+                SizedBox(height: height*.023,),
               ],
             ),
-<<<<<<< HEAD
-            SizedBox(
-              height: height * .15,
             ),
-            Text(
-              """Stay Covered,Stay Connected
-                   With Us.""",
-              style: TextStyle(
-                  color: const Color(0xFF1E1E1E),
-                  fontSize: height * .016,
-                  fontWeight: FontWeight.w500),
-            ),
-            SizedBox(
-              height: height * .008,
-=======
-
-            Padding(
-              padding: const EdgeInsets.only(top: 55),
-              child: Text(
-                """Stay Covered,Stay Connected
-                   With Us.""",
-                style: TextStyle(color: Color(0xFF1E1E1E), fontSize: Height*0.017),
-              ),
->>>>>>> origin/main
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => SignIn()));
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.orange, // Set the button color to orange
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                      height * .30), // Add rounded corners
-                ),
-              ),
-              child: const Text(
-                "Let's Go!",
-                style: TextStyle(color:Colors.white,fontWeight: FontWeight.w700),
-              ),
-            ),
-            SizedBox(
-              height: height * .023,
-            ),
-          ],
-        ),
-      ),
-    );
+        );
   }
 }
