cab666f8653e50e0b7eb435e1593cc5fa136ce77
midlajm799 <midlajm799@gmail.com>
2024-02-02 11:36:35 +0530

    3

--- lib/otp.dart
+++ lib/otp.dart
@@ -1,45 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
 
-class Otp extends StatelessWidget {
+class Otp extends StatefulWidget {
   const Otp({super.key});
 
   @override
+  State<Otp> createState() => _OtpState();
+}
+
+class _OtpState extends State<Otp> {
+  final pinController = TextEditingController();
+  final focusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
+  @override
   Widget build(BuildContext context) {
+
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: const TextStyle(
+        fontSize: 22,
+        color: Colors.black,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: Colors.black),
+      ),
+    );
     final Height = MediaQuery.of(context).size.height;
     final Width = MediaQuery.of(context).size.height;
-    return Scaffold(backgroundColor: Colors.white,
-      body: Container(
-        height: Height,
-        width: Width,
-        child: Column(
-            children: [
-              SizedBox(height: Height*0.1,),
-        Center(
-          child: Container(
-              height: Height / 3, child: Image.asset('assets/otp.jpg')),
-        ),
-              Row(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          height: Height,
+          width: Width,
+          child: Column(children: [
+            SizedBox(
+              height: Height * 0.1,
+            ),
+            Center(
+              child: Container(
+                  height: Height / 3, child: Image.asset('assets/otp.jpg')),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: Width * 0.06,right: Width*.1),
+              child: Column(
                 children: [
-                  Text('Otp verification',style: TextStyle(fontSize: Height/30),),
+                  Row(
+                    children: [
+                      Text(
+                        'Otp verification',
+                        style: TextStyle(fontSize: Height / 30),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: Height * 0.006),
+                  Divider(
+                    color: Colors.grey,
+                    height: Height * 0.001,
+                    thickness: 2,
+                    indent: 2,
+                    endIndent: 170,
+                  ),
+                  SizedBox(
+                    height: Height * 0.01,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Enter the verification code that\nwe just send you!',
+                        style: TextStyle(fontSize: Height / 60),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: Height*.05,),
+                 Pinput(controller: pinController,
+                   defaultPinTheme: defaultPinTheme,
+                   focusNode: focusNode,
+                   androidSmsAutofillMethod:
+                   AndroidSmsAutofillMethod.smsUserConsentApi,
+                   listenForMultipleSmsOnAndroid: true,
+                   // separatorBuilder: (index) => const SizedBox(width: 8),
+                   // validator: (value) {
+                   //   return value == '2222' ? null : 'Pin is incorrect';
+                   // },
+                   )
                 ],
               ),
-      ]
-
-
-
-
-
-
-
+            ),
+          ]),
         ),
       ),
-
-
-
-
-
-
-
     );
   }
 }
